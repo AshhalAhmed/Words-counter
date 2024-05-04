24ef41546281d1ce415aae1a6061f994a5fc23a7
ASHHAL AHMED <Ashhalahmed8@gmail.com>
2024-05-05 02:50:38 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let wordcounter = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter your sentence"
+    }
+]);
+const words = wordcounter.sentence.trim().split(" ");
+console.log(words);
+console.log(`your sentence words is: ` + `${words.length}`);
